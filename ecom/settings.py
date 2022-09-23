b8a048177b5d25a1c6378867e357b989d4b08261
Vinayaka-Doozie <vinayakadayanand007@gmail.com>
2022-09-23 18:45:14 +0530

    deployment changes

--- ecom/settings.py
+++ ecom/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
 
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
